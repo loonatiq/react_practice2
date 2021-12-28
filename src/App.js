920759957a487ab96ab1d4864c1712f02cde4f10
loonatiq <86408147+loonatiq@users.noreply.github.com>
2021-12-28 21:49:27 +0900

    3rdCommit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import HandIcon from "./HandIcon";
+import HandButton from './HandButton';
+import Button from './Button';
+
 
 function App() {
+  const handleClick = (value) => console.log(value);
+  const handleClearButton = () => console.log('처음부터');
   return (
+    <>
     <div>
-      <HandIcon value="rock" />
-      <HandIcon value="scissors" />
-      <HandIcon value="paper" />
+      <Button onClick={handleClearButton}>처음부터</Button>
     </div>
+    <HandButton value="rock" onClick={handleClick}/>
+    <HandButton value="scissors" onClick={handleClick}/>
+    <HandButton value="paper" onClick={handleClick}/>
+    </> 
   );
 }
 
